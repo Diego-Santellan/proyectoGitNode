cbaa3d558738fa6e5b43dde7b9de52202b3ca887
Diego Santellan <diego.santellan2001@gmail.com>
2023-05-15 22:44:55 -0300

    x

--- src/server.js
+++ src/server.js
@@ -8,5 +8,9 @@ app.use(express.static("public"));
 app.listen(PORT, () =>console.log(`Server is listening on port ${PORT}`));
 
 app.get("/", (req, res)=>{
-    res.sendFile("./index.html")
+    res.sendFile("index.html")
+});
+
+app.get("/user", (req, res)=>{
+    res.send({name:"pepe", age:30})
 });
